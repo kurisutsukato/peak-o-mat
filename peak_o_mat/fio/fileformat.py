1f2bf8197af6bab55bce15c7246f8953f7bfbe9a
ck <ckkart@gmail.com>
2019-02-25 19:39:42 +0000

    still improving data file parser

--- peak_o_mat/fio/fileformat.py
+++ peak_o_mat/fio/fileformat.py
@@ -7,7 +7,7 @@ import re
 import numpy as np
 
 def asfloat(arg):
-    if arg.strip() == '' or '#' in arg:
+    if arg.rstrip() == '' or '#' in arg:
         return np.NaN
     else:
         return float(arg)
@@ -20,6 +20,7 @@ def guess_format(path):
 
     try:
         for enc in [None]+config.options('encodings'):
+            # None corresponds to the default platform encoding
             try:
                 f = open(path,encoding=enc)
             except IOError:
@@ -33,13 +34,14 @@ def guess_format(path):
                     f.seek(0)
                     raise Found
     except Found:
+        #print('encoding', enc)
         pass
     else:
         raise PomError('Cannot read \'%s\'. Unknown encoding.'%path)
     finally:
         f.close()
 
-    text = rawdata.strip().split('\n')
+    text = rawdata.rstrip().split('\n')
 
     delimiters = ['\t',r'\s+',';']
     fp = config.getboolean('general','floating_point_is_comma')
@@ -56,58 +58,54 @@ def guess_format(path):
                 except IndexError:
                     break
                 # try to guess the delimiter
-                for delimiter in delimiters:
-                    try:
-                        assert len([float(x.strip()) for x in re.split(delimiter,line)]) >= 2
-                    except AssertionError:
-                        pass
-                    except ValueError:
-                        pass # non numeric data
-                    except IndexError:
-                        pass # less than 2 columns
-                    else:
-                        raise Found
+                for skipcol in range(2):
+                    for delimiter in delimiters:
+                        try:
+                            assert len([float(x.strip()) for x in re.split(delimiter,line)[skipcol:]]) >= 2
+                        except AssertionError:
+                            #print('asert')
+                            pass
+                        except ValueError:
+                            #print('value')
+                            pass # non numeric data
+                        except IndexError:
+                            #print('index')
+                            pass # less than 2 columns
+                        else:
+                            raise Found
             delimiters.remove(',')
-            text = rawdata.replace(',','.').strip().split('\n')
+            text = rawdata.replace(',','.').rstrip().split('\n')
             replace_comma = True
     except Found:
-        print('delimiter identified at row {}: {}'.format(row,delimiter))
-        print('floting comma:',replace_comma)
+        #print('delimiter identified at row {}: "{}"'.format(row,delimiter))
+        #print('floting comma:',replace_comma)
+        #print('has row label: {}'.format(bool(skipcol)))
         datastart = row
     else:
         raise PomError('Cannot parse \'%s\'. Unknown format.'%path)
 
     mat = re.compile(delimiter)
-    for line in text[datastart:]:
-        out = [x.strip() for x in mat.split(line.strip())]
-        if len(out) >= 2:
-            data.append(out)
 
-    # try to load labels:
-    line = text[datastart-1]
-
-    try:
-        [float(x.strip()) for x in mat.split(line.strip())]
-        collabels = [None]*len(data[0])
-    except ValueError:
-        collabels = [x.strip() for x in mat.split(line.strip())]
-    if len(collabels) == len(data[0])+1:
-        if collabels[0] in ['#',';','//']:
-            collabels.pop(0)
-    if len(collabels) != len(data[0]):
-        collabels = [None]*len(data[0])
-
-    data = [[asfloat(x) for x in mat.split(line.strip())] for line in text[datastart:]]
+    data = [[asfloat(x) for x in mat.split(line)[skipcol:]] for line in text[datastart:]]
+    data = np.asarray(data)
 
     if len(data) == 0:
         raise PomError('unable to parse %s'%path)
 
-    data = np.asarray(data)
-    print('found {} columns'.format(data.shape[1]))
-    print('encoding:', enc)
-    return
+    # try to load labels:
+    collabels = [x.strip() for x in mat.split(text[min(0,datastart-1)])]
+    has_collabels = len(collabels) == data.shape[1]+skipcol
+    print(len(collabels),data.shape[1],skipcol)
+    print(collabels)
+    #print('found {} columns'.format(data.shape[1]))
+    #print('encoding:', enc)
+
+    return path, enc, has_collabels, bool(skipcol), replace_comma, datastart, data.shape[1]
 
 if __name__ == '__main__':
-    guess_format('Untitled.csv')
-    guess_format('latin.txt')
-    guess_format('jap.txt')
+
+    print(guess_format('windows_utf.csv'))
+    print(guess_format('windows_utf-norowlabel.csv'))
+    print(guess_format('windows_utf-tab.csv'))
+    print(guess_format('windows_utf-tab-comma.csv'))
+    print(guess_format('windows_utf-tab-comma-nocollabel.csv'))
